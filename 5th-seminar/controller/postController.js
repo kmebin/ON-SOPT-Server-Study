8fd63408022b1a1786a08268b23b8af434d206e6
kmebin <blowhuiee@gmail.com>
2022-02-05 23:24:15 +0900

    [ADD] 모든 포스트 조회 API

--- 5th-seminar/controller/postController.js
+++ 5th-seminar/controller/postController.js
@@ -27,4 +27,36 @@ module.exports = {
       res.status(sc.INTERNAL_SERVER_ERROR).send(fail(sc.INTERNAL_SERVER_ERROR, rm.CREATE_POST_FAIL));
     }
   },
+  /**
+  * @route GET /post
+  * @desc 모든 포스트 조회
+  */
+  readAll: async (req, res) => {
+    try {
+      const posts = await postDB.findAll({
+        attributes:['id', 'title', 'content', 'createdAt'],
+        include: [{ 
+          model: userDB,
+          attributes: ['name'],
+        }],
+        raw: true,
+        nest: true,
+      });
+
+      if (!posts) return res.status(sc.BAD_REQUEST).send(fail(sc.BAD_REQUEST, rm.NO_POST));
+
+      const data = posts.map(item => ({
+        id: item.id,
+        title: item.title,
+        content: item.content,
+        name: item.user.name,
+        createdAt: item.createdAt,
+      }));
+
+      res.status(sc.OK).send(success(sc.OK, rm.READ_ALL_POSTS_SUCCESS, data));
+    } catch (error) {
+      console.log(error);
+      res.status(sc.INTERNAL_SERVER_ERROR).send(fail(sc.INTERNAL_SERVER_ERROR, rm.READ_ALL_POSTS_FAIL));
+    }
+  }
 };
--- 5th-seminar/routes/post/index.js
+++ 5th-seminar/routes/post/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const postController = require('../../controller/postController');
 
 router.post('/', postController.create);
+router.get('/', postController.readAll);
 
 module.exports = router;
