374d4fbf7ae527f0414a1f77d4e5d953b11a3882
kmebin <blowhuiee@gmail.com>
2022-02-09 17:33:20 +0900

    [UPDATE] 비동기 방식 삭제

--- 6th-seminar/lib/jwt.js
+++ 6th-seminar/lib/jwt.js
@@ -3,40 +3,36 @@ const { secretKey, options, refreshOptions } = require('../config/secretKey');
 const { TOKEN_EXPIRED, TOKEN_INVALID } = require('../constants/jwt');
 
 module.exports = {
-  sign: async (user) => {
+  sign: user => {
     const payload = {
       id: user.id,
       email: user.email,
       name: user.name
     };
     const result = {
-      token: jwt.sign(payload, secretKey, options),
+      accessToken: jwt.sign(payload, secretKey, options),
     };
 
     return result;
   },
-  verify: async (token) => {
+  verify: token => {
     let decoded;
 
     try {
       decoded = jwt.verify(token, secretKey);
-    } catch (err) {
-      if (err.message === 'jwt expired') {
-        console.log('expired token');
+    } catch (error) {
+      if (error.message === 'jwt expired') {
         return TOKEN_EXPIRED;
-      } else if (err.message === 'invalid token') {
-        console.log('invalid token');
-        console.log(TOKEN_INVALID);
+      } else if (error.message === 'invalid token') {
         return TOKEN_INVALID;
       } else {
-        console.log("invalid token");
         return TOKEN_INVALID;
       }
     }
 
     return decoded;
   },
-  createRefresh: async () => {
+  createRefresh: () => {
     const result = {
       refreshToken: jwt.sign({}, secretKey, refreshOptions),
     };
